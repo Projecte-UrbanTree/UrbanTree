cd95f5eb3388b29961547b82ef02f361a8592536
Ism1tha <xgalan@hotmail.com>
2024-11-06 22:18:08 +0100

    Add composer.lock to ignored files, mysql to mariadb.

--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,7 @@ require '../vendor/autoload.php';
 echo 'Testing our Docker setup for UrbanTree <br><br>';
 
 echo 'MySQL connection test: ';
-$mysql = new mysqli('mysql', 'demo_user', 'demo_pass', 'demo_db');
+$mysql = new mysqli('mariadb', 'demo_user', 'demo_pass', 'demo_db');
 $result = $mysql->query('SELECT * FROM demo_table');
 
 while ($row = $result->fetch_assoc()) {
