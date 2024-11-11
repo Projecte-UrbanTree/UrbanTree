73611b82eabe21814e9a8e94deb74cf5932fef99
Ism1tha <xgalan@hotmail.com>
2024-11-11 14:07:00 +0100

    Core files base

--- src/app/Core/Router.php
+++ src/app/Core/Router.php
@@ -1,3 +1,49 @@
 <?php
 
 namespace App\Core;
+
+class Router
+{
+    protected $routes = [];
+
+    public function load($file)
+    {
+        $routes = include $file;
+        $this->routes = $routes;
+    }
+
+    public function dispatch($requestMethod, $requestUri)
+    {
+        // Check for direct match first
+        if (isset($this->routes[$requestMethod][$requestUri])) {
+            return $this->callRoute($this->routes[$requestMethod][$requestUri]);
+        }
+
+        // If no direct match, check for dynamic routes with parameters
+        foreach ($this->routes[$requestMethod] as $route => $routeInfo) {
+            $routePattern = preg_replace('/:\w+/', '(\w+)', $route);
+            $pattern = '#^' . $routePattern . '$#';
+
+            if (preg_match($pattern, $requestUri, $matches)) {
+                array_shift($matches); // Remove the full match
+                $params = $this->extractParams($route, $matches);
+                return $this->callRoute($routeInfo, $params);
+            }
+        }
+
+        echo "404 Not Found";
+    }
+
+    protected function extractParams($route, $matches)
+    {
+        preg_match_all('/:(\w+)/', $route, $paramNames);
+        return array_combine($paramNames[1], $matches);
+    }
+
+    protected function callRoute($routeInfo, $params = [])
+    {
+        $controller = new $routeInfo['controller']();
+        $method = $routeInfo['method'];
+        $controller->$method(...array_values($params));
+    }
+}
--- src/app/Core/Session.php
+++ src/app/Core/Session.php
@@ -1,3 +1,41 @@
 <?php
 
 namespace App\Core;
+
+class Session
+{
+    public static function start()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_set_cookie_params([
+                'lifetime' => 0,
+                'path' => '/',
+                'domain' => $_SERVER['HTTP_HOST'],
+                'secure' => isset($_SERVER['HTTPS']),
+                'httponly' => true,
+                'samesite' => 'Strict'
+            ]);
+            session_start();
+        }
+    }
+    
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key)
+    {
+        return $_SESSION[$key] ?? null;
+    }
+
+    public static function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    public static function destroy()
+    {
+        session_destroy();
+    }
+}
