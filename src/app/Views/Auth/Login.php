c74aeaf05609868a4d70d850f74e3d511848eb04
Ism1tha <xgalan@hotmail.com>
2024-11-11 16:57:14 +0100

    Fix typo on login view

--- src/app/Views/Auth/Login.php
+++ src/app/Views/Auth/Login.php
@@ -2,7 +2,7 @@
 
 <form action="/login" method="POST" class="space-y-6">
     <div>
-        <label for="email" class="block text-sm font-medium text-gray-700">EmailAa</label>
+        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
         <input type="email" id="email" name="email" required class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="you@example.com">
     </div>
 
