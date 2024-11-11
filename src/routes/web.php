fae7d47fe4cf85d3cced3b26a6ca78945bc1cddb
Ism1tha <xgalan@hotmail.com>
2024-11-11 14:07:16 +0100

    Routes file content (base)

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,26 @@
 <?php
+
+/***
+ * Web Routes are defined here in this file and are loaded in the Router class
+ * All routes are defined in the following format:
+ * 
+ * METHOD => [
+ *     "URI" => [
+ *        "controller" => "ControllerName",
+ *        "method" => "methodName"
+ *    ]
+ * ]
+ * 
+ **/
+
+use App\Core\View;
+use App\Controllers\HomeController;
+
+return $routes = [
+    "GET" => [ 
+        "/" => [
+            "controller" => HomeController::class,
+            "method" => "index"
+        ],
+    ],
+];
