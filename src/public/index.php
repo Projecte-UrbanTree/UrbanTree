c5afd487d7478134d04a77faa29d4818b23875ac
0x1026 <69076992+0x1026@users.noreply.github.com>
2024-11-13 01:10:36 +0100

    refactor(structure): move routes to app folder

--- src/public/index.php
+++ src/public/index.php
@@ -5,5 +5,5 @@ require_once '../app/bootstrap.php';
 use App\Core\Router;
 
 $router = new Router();
-$router->load('../routes/web.php');
+$router->load('../app/routes.php');
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
