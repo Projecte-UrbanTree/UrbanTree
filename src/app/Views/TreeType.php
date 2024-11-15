b40edbd583f891b982c6bdc270658732ec5b90a5
SandraFerrando <sandraferrando@iesmontsia.org>
2024-11-15 15:44:11 +0100

    fix(tree-types): change variable name to follow the style

--- src/app/Views/TreeType.php
+++ src/app/Views/TreeType.php
@@ -9,12 +9,12 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($tree_types as $treeType): ?>
+            <?php foreach ($tree_types as $tree_type): ?>
                 <tr class="hover:bg-gray-50">
-                    <td class="px-4 py-2 border-b"><?php echo $treeType->getId(); ?></td>
-                    <td class="px-4 py-2 border-b"><?php echo $treeType->getFamily(); ?></td>
-                    <td class="px-4 py-2 border-b"><?php echo $treeType->getGenus(); ?></td>
-                    <td class="px-4 py-2 border-b"><?php echo $treeType->getSpecies(); ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $tree_type->getId(); ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $tree_type->getFamily(); ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $tree_type->getGenus(); ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $tree_type->getSpecies(); ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
