af5790c134d6bcc820e4d2228ec54c895fe9dca0
0x1026 <69076992+0x1026@users.noreply.github.com>
2024-11-16 22:48:55 +0100

    feat(element): update model and view to include point and zone relationships

--- src/app/Models/Element.php
+++ src/app/Models/Element.php
@@ -6,10 +6,9 @@ use App\Core\BaseModel;
 
 class Element extends BaseModel
 {
-    public int $zone_id;
     public string $name;
-    public float $latitude;
-    public float $longitude;
+    public int $zone_id;
+    public int $point_id;
     protected $created_at;
 
     protected static function getTableName()
@@ -21,12 +20,21 @@ class Element extends BaseModel
     {
         $element = new Element();
         $element->id = $data['id'];
-        $element->zone_id = $data['zone_id'];
         $element->name = $data['name'];
-        $element->latitude = $data['latitude'];
-        $element->longitude = $data['longitude'];
+        $element->zone_id = $data['zone_id'];
+        $element->point_id = $data['point_id'];
         $element->created_at = $data['created_at'];
         return $element;
+    }
+
+    public function zone()
+    {
+        return $this->belongsTo(Zone::class, 'zone_id');
+    }
+
+    public function point()
+    {
+        return $this->belongsTo(Point::class, 'point_id');
     }
 
     public function getCreatedAt()
--- src/app/Views/Element.php
+++ src/app/Views/Element.php
@@ -4,8 +4,10 @@
             <tr class="bg-gray-100 text-left">
                 <th class="px-4 py-2 border-b">ID</th>
                 <th class="px-4 py-2 border-b">Name</th>
-                <th class="px-4 py-2 border-b">Latitud</th>
-                <th class="px-4 py-2 border-b">Longitud</th>
+                <th class="px-4 py-2 border-b">Zone Name</th>
+                <th class="px-4 py-2 border-b">Zone Postal Code</th>
+                <th class="px-4 py-2 border-b">Latitude</th>
+                <th class="px-4 py-2 border-b">Longitude</th>
                 <th class="px-4 py-2 border-b">Created At</th>
             </tr>
         </thead>
@@ -14,8 +16,10 @@
                 <tr class="hover:bg-gray-50">
                     <td class="px-4 py-2 border-b"><?php echo $element->getId(); ?></td>
                     <td class="px-4 py-2 border-b"><?php echo $element->name; ?></td>
-                    <td class="px-4 py-2 border-b"><?php echo $element->latitude; ?></td>
-                    <td class="px-4 py-2 border-b"><?php echo $element->longitude; ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $element->zone()->name; ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $element->zone()->postal_code; ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $element->point()->latitude; ?></td>
+                    <td class="px-4 py-2 border-b"><?php echo $element->point()->longitude; ?></td>
                     <td class="px-4 py-2 border-b"><?php echo $element->getCreatedAt(); ?></td>
                 </tr>
             <?php endforeach; ?>
