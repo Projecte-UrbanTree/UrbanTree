5082041cd35c806d73476c2600e10e8a50e0b3e1
oriolreverte36 <oreverte@avanzaprl.es>
2024-11-18 01:04:52 +0100

    feat: new views created to view the incidents and be able to create TODO: the method to create a new incident needs to be updated Added a new get method in the controller to obtain all the elements

---
+++ src/app/Views/Incidence/SeeAllIncidences.php
@@ -0,0 +1,27 @@
+<div class="overflow-x-auto px-8 py-6 bg-gray-50 rounded-lg shadow-lg">
+    <h1 class="text-3xl font-extrabold text-center text-gray-800 mb-6">Todas las Incidencias</h1>
+    <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
+        <thead>
+            <tr class="bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
+                
+                <th class="px-6 py-4 text-left font-semibold">Element Name</th>
+                <th class="px-6 py-4 text-left font-semibold">Name</th>
+                <th class="px-6 py-4 text-left font-semibold">Description</th>
+                <th class="px-6 py-4 text-left font-semibold">Photo</th>
+                <th class="px-6 py-4 text-left font-semibold">Incident Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($incidences as $incidence): ?>
+                <tr class="hover:bg-gray-50 transition-all duration-200 ease-in-out">
+                    
+                    <td class="px-6 py-4 border-b text-sm text-gray-700"><?php echo $incidence->name; ?></td>
+                    <td class="px-6 py-4 border-b text-sm text-gray-700"><?php echo $incidence->element()->name; ?></td>
+                    <td class="px-6 py-4 border-b text-sm text-gray-700"><?php echo $incidence->description; ?></td>
+                    <td class="px-6 py-4 border-b text-sm text-gray-700"><?php echo $incidence->photo ? $incidence->photo : 'No photo'; ?></td>
+                    <td class="px-6 py-4 border-b text-sm text-gray-700"><?php echo $incidence->getCreatedAt(); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
