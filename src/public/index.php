e8c6eb5d9041d4de3c602d3c67d4b1ea635f6b0b
Ism1tha <xgalan@hotmail.com>
2024-11-18 19:00:07 +0100

    fix(routes): :ambulance: routes path not updated on last commit

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../app/bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../app/bootstrap.php';
 
 use App\Core\Router;
 
-$router = new Router;
-$router->load('../app/routes.php');
+$router = new Router();
+$router->load('../routes/web.php');
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $_POST);
