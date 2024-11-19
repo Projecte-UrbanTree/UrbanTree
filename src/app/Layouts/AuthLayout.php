9c46340950e6ca458e86470144d96a460f039e12
24Victor <borenvictorzheng@iesmontsia.org>
2024-11-19 20:15:31 +0100

    feat: add-assets

--- src/app/Layouts/AuthLayout.php
+++ src/app/Layouts/AuthLayout.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?php echo htmlspecialchars($title).' - '.htmlspecialchars(getenv('APP_NAME')); ?>
+        <?php echo htmlspecialchars($title) . ' - ' . htmlspecialchars(getenv('APP_NAME')); ?>
     </title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-100 font-sans leading-normal tracking-normal flex items-center justify-center h-screen">
@@ -17,7 +17,8 @@
         <!-- Header -->
         <header class="text-center mb-8">
             <h1 class="text-2xl font-extrabold text-gray-800">
-                <?php echo htmlspecialchars($title); ?></h1>
+                <?php echo htmlspecialchars($title); ?>
+            </h1>
         </header>
 
         <!-- Main content -->
