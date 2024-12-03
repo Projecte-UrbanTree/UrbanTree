ed89415d5279d30a62fc09604579d6468b3f9084
0x1026 <69076992+0x1026@users.noreply.github.com>
2024-12-03 23:44:24 +0100

    refactor(api): clean up commented requirements.txt and code in app.py

--- api/src/app.py
+++ api/src/app.py
@@ -25,17 +25,3 @@ def on_startup():
 @app.get("/")
 def hello():
     return "Hello, Docker!"
-
-# @app.post("/heroes/")
-# def create_hero(hero: Hero):
-#     with Session(engine) as session:
-#         session.add(hero)
-#         session.commit()
-#         session.refresh(hero)
-#         return hero
-
-# @app.get("/heroes/")
-# def read_heroes():
-#     with Session(engine) as session:
-#         heroes = session.exec(select(Hero)).all()
-#         return heroes
