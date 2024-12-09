fb1048fe577b414bd66e45d9c8374a80a841cc9b
SandraFerrando <sandraferrando@iesmontsia.org>
2024-12-09 16:40:21 +0100

    fix(tree-types): update links

--- src/app/Views/Admin/TreeType/Create.php
+++ src/app/Views/Admin/TreeType/Create.php
@@ -1,5 +1,5 @@
 <div class="mb-4 flex justify-end">
-    <a href="/tree-types"
+    <a href="/admin/tree-types"
         class="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg shadow focus:outline-none focus:ring focus:ring-green-500 flex items-center space-x-2">
         <!-- Heroicon for return/back (chevron-left) -->
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
