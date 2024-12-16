babac892d48654bb25ee96c54732632856b2f1f5
elbacalau <oriolreverte36@gmail.com>
2024-12-16 17:35:56 +0100

    feat: enhance AccountConfig view with personal information section and Spanish localization

--- app/src/app/Views/Admin/AccountConfig/AccountConfig.php
+++ app/src/app/Views/Admin/AccountConfig/AccountConfig.php
@@ -1,3 +1,20 @@
-<div class="">
-    <p>works!</p>
+<div class="mt-8">
+    <h1 class="text-5xl font-semibold">Configuración</h1>
+    <p class="text-md text-gray-600 mt-3">Aquí podrás configurar tu cuenta.</p>
+
+    <article class="mt-8">
+        <h2 class="text-3xl font-semibold">Información personal</h2>
+
+        <!-- avatar info -->
+        <div class="p-4 mt-4 bg-white shadow-md rounded-md w-fit flex items-center gap-4">
+            <img class="h-25 w-25 rounded-full" src="/assets/images/avatar.png" alt="User Avatar">
+            <div class="flex flex-col items-center">
+                <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">Cambiar imagen</button>
+                <p class="text-sm text-gray-600 mt-2">JPG or PNG. 1MB max.</p>
+            </div>
+        </div>
+
+
+    </article>
+
 </div>
