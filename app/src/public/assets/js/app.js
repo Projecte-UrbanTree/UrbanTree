b6a94120d0a0140045806b1d5375ee898305040e
0x1026 <69076992+0x1026@users.noreply.github.com>
2024-12-20 03:38:14 +0100

    refactor: simplify javascript validations converting to POO

--- app/src/public/assets/js/app.js
+++ app/src/public/assets/js/app.js
@@ -1,177 +1,178 @@
-const errorMessagesDiv = document.getElementById("errorMessages");
+class Validator {
+    constructor(errorMessagesDiv) {
+        this.errorMessagesDiv = errorMessagesDiv;
+        this.regexPatterns = [
+            {
+                name: "lettersAndSpaces",
+                regex: /^[a-zA-Z\s]+$/,
+                explanation: "El campo solo puede contener letras y espacios.",
+            },
+            {
+                name: "numbersOnly",
+                regex: /^\d+([eE][+-]?\d+)?$/,
+                explanation: "El campo solo puede contener números o notación científica.",
+            },
+            {
+                name: "numbersWithTwoDecimals",
+                regex: /^\d+(\.\d{1,2})?([eE][+-]?\d+)?$/,
+                explanation: "El campo solo puede contener números con hasta dos decimales o notación científica.",
+            },
+        ];
+    }
 
-const regexPatterns = [
-    {
-        name: "lettersAndSpaces",
-        regex: /^[a-zA-Z\s]+$/,
-        explanation: "El campo solo puede contener letras y espacios.",
-    },
-    {
-        name: "numbersOnly",
-        regex: /^\d+$/,
-        explanation: "El campo solo puede contener números.",
-    },
-];
+    createErrorElement(message) {
+        const errorMsg = document.createElement("p");
+        errorMsg.textContent = message;
+        this.errorMessagesDiv.appendChild(errorMsg);
+        return false;
+    }
 
-function createErrorElement(message) {
-    const errorMsg = document.createElement("p");
-    errorMsg.textContent = message;
-    errorMessagesDiv.appendChild(errorMsg);
-    return false;
+    validateEmptyField(value, fieldName) {
+        return !value
+            ? this.createErrorElement(
+                  `${fieldName}: El campo no puede estar vacío.`
+              )
+            : true;
+    }
+
+    validateField(value, regexName, fieldName) {
+        const pattern = this.regexPatterns.find(
+            (pattern) => pattern.name === regexName
+        );
+        const explanation = pattern
+            ? pattern.explanation
+            : "El valor no es válido.";
+        return pattern && !pattern.regex.test(value)
+            ? this.createErrorElement(`${fieldName}: ${explanation}`)
+            : true;
+    }
+
+    dateCannotBeAfter(startDate, endDate, fieldName) {
+        return new Date(startDate) > new Date(endDate)
+            ? this.createErrorElement(
+                  `${fieldName}: La fecha inicial no puede ser posterior a la fecha final.`
+              )
+            : true;
+    }
+
+    validatePositiveNumber(value, fieldName) {
+        return parseFloat(value) > 0
+            ? true
+            : this.createErrorElement(
+                  `${fieldName}: El valor debe ser un número positivo.`
+              );
+    }
+
+    validateMaxValue(value, max, fieldName) {
+        return parseFloat(value) <= max
+            ? true
+            : this.createErrorElement(
+                  `${fieldName}: El valor no puede ser mayor a ${max}.`
+              );
+    }
 }
 
-/**
- * Validate if a field is empty and return an error message if it is
- */
-function validateEmptyField(value, fieldName) {
-    return !value
-        ? createErrorElement(`${fieldName}: El campo no puede estar vacío.`)
-        : true;
-}
+class FormValidator {
+    constructor(formId, fields) {
+        this.form = document.getElementById(formId);
+        this.fields = fields;
+        this.errorMessagesDiv = document.getElementById("errorMessages");
+        this.validator = new Validator(this.errorMessagesDiv);
 
-/**
- * Validate a field based on a regex pattern name and return an error message if invalid
- */
-function validateField(value, regexName, fieldName) {
-    const pattern = regexPatterns.find((pattern) => pattern.name === regexName);
-    const explanation = pattern
-        ? pattern.explanation
-        : "El valor no es válido.";
-    return pattern && !pattern.regex.test(value)
-        ? createErrorElement(`${fieldName}: ${explanation}`)
-        : true;
-}
-
-/**
- * Validate that one date is not after another date
- */
-function dateCannotBeAfter(startDate, endDate, fieldName) {
-    return new Date(startDate) > new Date(endDate)
-        ? createErrorElement(
-              `${fieldName}: La fecha inicial no puede ser posterior a la fecha final.`
-          )
-        : true;
-}
-
-/**
- * Validate that a field contains a positive integer
- */
-function validatePositiveInteger(value, fieldName) {
-    const pattern = /^[+]?\d+([eE][+]?\d+)?$/;
-    return pattern.test(value) && parseFloat(value) > 0
-        ? true
-        : createErrorElement(
-              `${fieldName}: El valor debe ser un número entero positivo.`
-          );
-}
-
-/**
- * Validate that a field does not exceed the maximum value
- */
-function validateMaxValue(value, max, fieldName) {
-    return parseFloat(value) <= max
-        ? true
-        : createErrorElement(
-              `${fieldName}: El valor no puede ser mayor a ${max}.`
-          );
-}
-
-function getFieldName(fieldId) {
-    const label = document.querySelector(`label[for="${fieldId}"]`);
-    return label ? label.textContent.trim() : fieldId;
-}
-
-function validateForm(event, fields) {
-    errorMessagesDiv.innerHTML = "";
-    errorMessagesDiv.classList.add("hidden");
-
-    let isValid = true;
-    fields.forEach((field) => {
-        const element = document.getElementById(field.id);
-        const value = element ? element.value.trim() : null;
-        const fieldName = getFieldName(field.id);
-        const max = element ? parseFloat(element.max) : null;
-
-        for (const check of field.checks) {
-            let validation;
-            switch (check.type) {
-                case "empty":
-                    validation = validateEmptyField(value, fieldName);
-                    break;
-                case "regex":
-                    validation = validateField(
-                        value,
-                        check.regexName,
-                        fieldName
-                    );
-                    break;
-                case "daterange":
-                    const startDate = document
-                        .getElementById(check.startDateId)
-                        .value.trim();
-                    const endDate = document
-                        .getElementById(check.endDateId)
-                        .value.trim();
-                    validation = dateCannotBeAfter(
-                        startDate,
-                        endDate,
-                        fieldName
-                    );
-                    break;
-                case "positiveInteger":
-                    validation = validatePositiveInteger(value, fieldName);
-                    break;
-                case "maxValue":
-                    validation = validateMaxValue(value, max, fieldName);
-                    break;
-                default:
-                    validation = true;
-            }
-            if (!validation) {
-                isValid = false;
-                break;
-            }
+        if (this.form) {
+            this.form.addEventListener("submit", (event) =>
+                this.validateForm(event)
+            );
         }
-    });
-
-    if (!isValid) {
-        event.preventDefault();
-        errorMessagesDiv.classList.remove("hidden");
     }
-}
 
-function addFormValidation(formId, fields) {
-    const form = document.getElementById(formId);
-    if (form) {
-        form.addEventListener("submit", (event) => {
-            validateForm(event, fields);
+    getFieldName(fieldId) {
+        const label = document.querySelector(`label[for="${fieldId}"]`);
+        return label ? label.textContent.trim() : fieldId;
+    }
+
+    validateForm(event) {
+        this.errorMessagesDiv.innerHTML = "";
+        this.errorMessagesDiv.classList.add("hidden");
+
+        let isValid = true;
+        this.fields.forEach((field) => {
+            const element = document.getElementById(field.id);
+            const value = element ? element.value.trim() : null;
+            const fieldName = this.getFieldName(field.id);
+            const max = element ? parseFloat(element.max) : null;
+
+            for (const check of field.checks) {
+                let validation;
+                switch (check.type) {
+                    case "empty":
+                        validation = this.validator.validateEmptyField(
+                            value,
+                            fieldName
+                        );
+                        break;
+                    case "regex":
+                        validation = this.validator.validateField(
+                            value,
+                            check.regexName,
+                            fieldName
+                        );
+                        break;
+                    case "daterange":
+                        const startDate = document
+                            .getElementById(check.startDateId)
+                            .value.trim();
+                        const endDate = document
+                            .getElementById(check.endDateId)
+                            .value.trim();
+                        validation = this.validator.dateCannotBeAfter(
+                            startDate,
+                            endDate,
+                            fieldName
+                        );
+                        break;
+                    case "positiveNumber":
+                        validation = this.validator.validatePositiveNumber(
+                            value,
+                            fieldName
+                        );
+                        break;
+                    case "maxValue":
+                        validation = this.validator.validateMaxValue(
+                            value,
+                            max,
+                            fieldName
+                        );
+                        break;
+                    default:
+                        validation = true;
+                }
+                if (!validation) {
+                    isValid = false;
+                    break;
+                }
+            }
         });
+
+        if (!isValid) {
+            event.preventDefault();
+            this.errorMessagesDiv.classList.remove("hidden");
+        }
     }
 }
 
-/**
- * Event listeners for form validation
- */
-addFormValidation("contractForm", [
+// Initialize form validations
+new FormValidator("contractForm", [
     {
         id: "name",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "regex",
-                regexName: "lettersAndSpaces",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "lettersAndSpaces" },
         ],
     },
     {
         id: "start_date",
-        checks: [
-            {
-                type: "empty",
-            },
-        ],
+        checks: [{ type: "empty" }],
     },
     {
         id: "end_date",
@@ -186,82 +187,52 @@ addFormValidation("contractForm", [
     {
         id: "invoice_proposed",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "positiveInteger",
-            },
-            {
-                type: "maxValue",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "numbersWithTwoDecimals" },
+            { type: "positiveNumber" },
+            { type: "maxValue" },
         ],
     },
     {
         id: "invoice_agreed",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "positiveInteger",
-            },
-            {
-                type: "maxValue",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "numbersWithTwoDecimals" },
+            { type: "positiveNumber" },
+            { type: "maxValue" },
         ],
     },
     {
         id: "invoice_paid",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "positiveInteger",
-            },
-            {
-                type: "maxValue",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "numbersWithTwoDecimals" },
+            { type: "positiveNumber" },
+            { type: "maxValue" },
         ],
     },
 ]);
 
-addFormValidation("treeTypeForm", [
+new FormValidator("treeTypeForm", [
     {
         id: "family",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "regex",
-                regexName: "lettersAndSpaces",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "lettersAndSpaces" },
         ],
     },
     {
         id: "genus",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "regex",
-                regexName: "lettersAndSpaces",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "lettersAndSpaces" },
         ],
     },
     {
         id: "species",
         checks: [
-            {
-                type: "empty",
-            },
-            {
-                type: "regex",
-                regexName: "lettersAndSpaces",
-            },
+            { type: "empty" },
+            { type: "regex", regexName: "lettersAndSpaces" },
         ],
     },
 ]);
