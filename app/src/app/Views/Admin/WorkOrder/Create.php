b5c067123ce5990a6f63f671b8cd9550b9f57512
Developer1981Duck <106682302+Developer1981Duck@users.noreply.github.com>
2025-01-02 16:13:56 +0100

    feat:work-orders

--- app/src/app/Views/Admin/WorkOrder/Create.php
+++ app/src/app/Views/Admin/WorkOrder/Create.php
@@ -26,7 +26,7 @@
         </div>
 
         <div class="flex justify-end">
-            <button id="addBlocks"
+            <button type="button" id="addBlocks" onclick="addBlock()"
                 class="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg shadow focus:outline-none focus:ring focus:ring-green-500">
                 Agregar Bloques
             </button>
@@ -34,7 +34,16 @@
 
         <div id="blocksContainer" class="space-y-6">
             <div class="border border-gray-300 rounded-lg shadow p-4 bg-gray-50">
-                <h3 class="text-lg font-semibold text-gray-800 mb-4">Bloque 1</h3>
+                <div class="flex justify-between items-center">
+                    <h3 class="text-lg font-semibold text-gray-800 mb-4">Bloque 1</h3>
+                    <button type="button" onclick="removeBlock(this)" class="text-red-500 hover:text-red-700 focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </button>
+                </div>      
                 <div>
                     <label for="zones" class="block text-sm font-medium text-gray-700 mb-1">Zonas</label>
                     <input type="text" id="zonesInput" readonly onclick="openModal('modalZones')"
@@ -42,16 +51,16 @@
                         class="w-full px-4 py-2 border border-gray-300 rounded-lg bg-gray-100 cursor-pointer focus:outline-none">
                 </div>
                 <div class="flex justify-end mt-4">
-                    <button id="addTask"
+                    <button type="button" id="addTask" onclick="addTask(this)"
                         class="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-lg shadow focus:outline-none focus:ring focus:ring-green-500">
                         Agregar Tareas
                     </button>
                 </div>
-                <div id="tasksContainer" class="space-y-4">
+                <div class="tasksContainer space-y-4">
+                    <h3 class="text-lg font-semibold text-gray-800 mb-2">Seleccionar Tareas</h3>
                     <div class="task-row flex space-x-4 items-end">
                         <!-- Dropdown Task Type -->
                         <div class="w-1/2">
-                            <h3 class="text-lg font-semibold text-gray-800 mb-2">Seleccionar Tareas</h3>
                             <select name="taskType" id="taskType"
                                 class="w-full px-4 py-2 border border-gray-300 rounded-lg bg-white focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
                                 <option value="" disabled selected>Seleccione una tarea</option>
@@ -86,10 +95,6 @@
                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
                         </button>
-
-                        <div id="tasksContainer" class="space-y-4 mb-4">
-                            <!-- Tasks is added dynamic-->
-                        </div>
                     </div>
                     <div>
 
