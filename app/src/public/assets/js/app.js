47e1190956db00682748f6bfee4276de51eddca8
24Victor <borenvictorzheng@iesmontsia.org>
2025-01-08 19:44:51 +0100

    feat/work-order edit

--- app/src/public/assets/js/app.js
+++ app/src/public/assets/js/app.js
@@ -388,20 +388,12 @@ function saveSelection(modalId) {
 
 let taskTypeOptions = `<option value="" disabled selected>Seleccione una tarea</option>`;
 taskTypes.forEach((task_type) => {
-    taskTypeOptions += `
-        <option value="${task_type.id}">
-            ${task_type.name}
-        </option>
-    `;
+    taskTypeOptions += `<option value="${task_type.id}">${task_type.name}</option>`;
 });
 
 let speciesOptions = `<option value="" selected>Opcional</option>`;
 treeTypes.forEach((tree_type) => {
-    speciesOptions += `
-        <option value="${tree_type.id}">
-            ${tree_type.species}
-        </option>
-    `;
+    speciesOptions += `<option value="${tree_type.id}">${tree_type.species}</option>`;
 });
 
 function addBlock() {
@@ -437,6 +429,28 @@ function addBlock() {
         </div>
         <div class="tasksContainer space-y-4">
             <h3 class="text-lg font-semibold text-gray-800 my-3">Seleccionar Tareas</h3>
+                <div class="task-row flex space-x-4 items-end" data-task-index="0">
+                <div class="flex-auto">
+                    <select name="blocks[${blockCount}][tasks][0][taskType]"
+                        class="w-full px-4 py-2 border border-gray-300 rounded-lg bg-white focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
+                        ${taskTypeOptions}
+                    </select>
+                </div>
+                <div class="flex-auto flex items-center space-x-2">
+                    <span class="block text-lg font-semibold text-gray-800">Species</span>
+                    <select name="blocks[${blockCount}][tasks][0][species]"
+                        class="flex-1 px-4 py-2 border border-gray-300 rounded-lg bg-white focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
+                        ${speciesOptions}
+                    </select>
+                </div>
+                <button type="button" onclick="removeTaskRow(this)"
+                    class="text-red-500 hover:text-red-700 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                </button>
             </div>
         </div>
         <button type="button" onclick="addTask(this)" class="btn-create mt-4">
@@ -450,7 +464,6 @@ function addBlock() {
     `;
 
     blocksContainer.appendChild(block);
-    addTask(block.querySelector(".btn-create"));
     updateBlock();
 }
 
