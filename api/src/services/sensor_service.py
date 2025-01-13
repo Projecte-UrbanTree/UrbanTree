97ec2e07d1e4adc0b6ddfa8cd1bffac23388db6f
elbacalau <oriolreverte36@gmail.com>
2025-01-13 17:58:28 +0100

    refactor: update example environment variables and import paths in sensor service

--- api/src/services/sensor_service.py
+++ api/src/services/sensor_service.py
@@ -2,7 +2,7 @@ from sentry_sdk.crons import monitor
 from sqlmodel import select
 
 from src.database import get_session
-from api.src.models.sensor_model import SensorHistory
+from src.models.sensor_model import SensorHistory
 from src.utils.file_loader import load_json_file
 
 
